91a613d020c5764d83bf70599007c06ad5cb141c
colinlevine <89605627+colinlevine@users.noreply.github.com>
2023-08-08 23:01:15 -0500

    Add files via upload

---
+++ public/client.js
@@ -0,0 +1,74 @@
+const socket = io("/");
+
+// peerjs
+const myPeer = new Peer(undefined, {
+    // host: "/",
+    // port: "3001"
+});
+
+myPeer.on('open', clientUserID => {
+    socket.emit("join-room", clientRoomID, clientUserID);
+});
+
+
+// Video
+const videoGrid = document.querySelector("#video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    // Add primary user video
+    addVideo(myVideo, stream)
+
+    // Add secondary
+    myPeer.on('call', call => {
+        call.answer(stream) // Adds second video to user 1
+        const video = document.createElement("video");
+        video.style.marginLeft = "2.5rem";
+        call.on('stream', userVideoStream => { // Updates user 2 with user 1 video
+            addVideo(video, userVideoStream)
+        })
+    });
+
+    socket.on("user-connected", userID => {
+        const call = myPeer.call(userID, stream);
+        const video = document.createElement("video");
+        video.style.marginLeft = "2.5rem";
+        call.on("stream", userVideoStream => {
+            addVideo(video, userVideoStream);
+        })
+        call.on('close', () => {
+            video.remove()
+        })
+
+        peers[userID] = call;
+    });
+
+    socket.on("user-disconnected", userID => {
+        if (peers[userID]) {
+            peers[userID].close();
+        }
+    })
+
+    socket.on("public-room", () => {
+        document.querySelector("#room-status").innerText = "Public Room";
+    })
+    socket.on("private-room", () => {
+        document.querySelector("#room-status").innerText = "Private Room";
+    })
+    
+});
+
+function addVideo (myVideo, stream) {
+    myVideo.srcObject = stream;
+    myVideo.addEventListener('loadedmetadata', () => {
+        myVideo.play();
+    });
+    videoGrid.append(myVideo);
+}
+
---
+++ server.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const { allowedNodeEnvironmentFlags } = require("process");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4 } = require("uuid")
+const port = process.env.PORT || 3000;
+var allRooms = [];
+var publicRooms = [];
+var publicID = null;
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    console.log("Public rooms: " + publicRooms);
+    res.redirect(`/${v4()}`) // creates room id
+});
+
+app.get("/:room", (req, res) => {
+    res.render('room', { roomID: req.params.room, allRooms})
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomID, userID) => {
+        // console.log(roomID, userID);
+        publicID = roomID;
+
+        // prevents duplicate rooms
+        var x = allRooms.indexOf(roomID);
+        if (x >= 0) {   
+            // console.log("Room exists");
+        }
+        else {
+            allRooms.push(roomID); // Adds room id to list of open rooms
+        }
+        // console.log("All rooms: " + allRooms);
+
+        
+        console.log(allRooms);
+        socket.join(roomID);
+        socket.to(roomID).emit("user-connected", userID);
+
+        socket.on("disconnect", (req, res) => {
+            socket.to(roomID).emit("user-disconnected", userID);
+            // Removes user id from list when a user disconnects
+            const index = allRooms.indexOf(roomID)
+            allRooms.splice(index, 1);
+            const index2 = publicRooms.indexOf(roomID)
+            publicRooms.splice(index2, 1);
+            console.log(allRooms);
+            io.sockets.emit("private-room");
+        });
+    })
+});
+
+// Form to join a room from room id
+app.use(express.urlencoded({ extended: true }));
+app.post("/join", (req, res) => {
+    var newRoomID = req.body["updateRoom"];
+    res.redirect(newRoomID);
+});
+
+app.post("/publicRoom", (req,res) => {
+    // prevents duplicate rooms
+    var x = publicRooms.indexOf(publicID);
+    if (x >= 0) {   
+        // console.log("Room exists")
+    }
+    else {
+        publicRooms.push(publicID);
+        io.sockets.emit("public-room");
+    }
+    console.log("Public rooms: " + publicRooms);
+    });
+
+app.post("/privateRoom", () => {
+    // prevents duplicate rooms
+    var y = publicRooms.indexOf(publicID);
+    if (y >= 0) {   
+        publicRooms.splice(y, 1);
+        io.sockets.emit("private-room");
+    }
+
+    console.log("Public rooms: " + publicRooms);
+});
+
+// Join a random room
+app.post("/joinRandom", (req, res) => {
+    publicRoomsIndex = Math.floor(Math.random() * publicRooms.length);
+    var newRoomID = publicRooms[publicRoomsIndex];
+    x = 0;
+    while (x == 0) {
+        if (newRoomID == publicID || newRoomID == null) {
+            publicRoomsIndex = Math.floor(Math.random() * publicRooms.length);
+            var newRoomID = publicRooms[publicRoomsIndex];
+        }
+        else{
+            break
+        }
+    }
+    
+    console.log("Public rooms: " + publicRooms);
+    if (publicRooms.length == 0) {
+        res.write("<p>No rooms found</p>");
+        res.write("<a href='/'>Back to main page</a><br>");
+        res.write('<link rel="stylesheet" href="style.css">');
+        res.end();
+    }
+    else {
+        res.redirect("/" + newRoomID);
+        // allRooms.splice(index, 1);
+        io.sockets.emit("public-room");
+    }
+    // console.log(allRooms);
+});
+
+
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}.`)
+});
